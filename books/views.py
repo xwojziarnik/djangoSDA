e9bb0799b1fdbb74bb03621b6f0ce3f58dc90a0e
xwojziarnik <woj.ziarnik@gmail.com>
2022-04-09 15:45:58 +0200

    uuids-a and uuids-b added

--- books/views.py
+++ books/views.py
@@ -1,3 +1,21 @@
+from uuid import uuid4
+
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+def get_hello(request: WSGIRequest) -> HttpResponse:
+    return HttpResponse("<h1>hello world</h1>")
+
+# 12. Utwórz funkcję zwracającą listę stringów. Stringi niech będą losowym UUID dodawanym do listy. Lista niech posiada 10 elementów.
+#
+#     a) Zwróć listę jako HTTPResponse (musisz na liście zrobić json.dumps)
+#     b) zwróć listę jako JsonResponse
+
+def get_uuids_a(request: WSGIRequest) -> HttpResponse:
+    uuids = [f"{uuid4()}" for _ in range(10)]
+    return HttpResponse(f"uuids={uuids}")
+
+def get_uuids_b(request: WSGIRequest) -> JsonResponse:
+    uuids = [f"{uuid4()}" for _ in range(10)]
+    return JsonResponse({"uuids":uuids})
--- library/urls.py
+++ library/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from books.views import get_hello, get_uuids_a, get_uuids_b
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_hello),
+    path('uuids-a', get_uuids_a),
+    path('uuids-b', get_uuids_b),
 ]
