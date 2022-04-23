ddd91a2bf0eeb6ab6d6a1174858cb6f436615b4e
xwojziarnik <woj.ziarnik@gmail.com>
2022-04-23 15:59:04 +0200

    added migrations and models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class BookAuthor(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.TextField()
+    authors = models.ManyToManyField(BookAuthor)
+    publisher = models.TextField(null=True)
+    published_date = models.DateField(null=True)
+    categories = models.ManyToManyField(Category)
+    average_rating = models.FloatField(null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.authors} - {self.published_date}"
