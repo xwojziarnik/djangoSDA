c3b60e6f0eed89c2540cc96e95940dcd2f2d0541
xwojziarnik <woj.ziarnik@gmail.com>
2022-04-24 15:52:33 +0200

    Added forms and book_form

--- books/forms.py
+++ books/forms.py
@@ -1,5 +1,7 @@
-from django.forms import Form, CharField
+from django.forms import Form, CharField, ModelForm
 import logging
+
+from books.models import BookAuthor, Book
 
 logger = logging.getLogger("forms_logger")
 
@@ -9,3 +11,35 @@ class CategoryForm(Form):
     def clean_name(self):
         logger.info(self.cleaned_data)
         return self.cleaned_data.get("name")
+
+class AuthorForm(ModelForm):
+    class Meta:
+        model = BookAuthor
+        exclude = []
+
+    name = CharField(min_length=3, max_length=256, required=True)
+
+    def clean(self):
+        result = super().clean()
+        logger.info(f"AuthorForm - clean run - result is {result}")
+        return result
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        # exclude = []
+        fields = [
+            "title",
+            "authors",
+            "publisher",
+            "published_date",
+            "categories",
+            "average_rating",
+        ]
+
+    title = CharField(min_length=3, max_length=256, required=True)
+
+    def clean(self):
+        result = super().clean()
+        logger.info(f"BookForm - clean run - result is {result}")
+        return result
