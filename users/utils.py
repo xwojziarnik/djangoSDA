ae6c51c7e5fae5055f29c094d09f257b886e85ec
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-14 14:30:53 +0200

    Verify password with salt

--- users/utils.py
+++ users/utils.py
@@ -24,3 +24,12 @@ def hash_password_with_salt(password: str) -> str:
 
     return combined_salt_and_password
 
+def verify_password_with_salt(password: str, hashed_password: str) -> bool:
+    hashed_salt = hashed_password[-64:]
+    new_password = password + hashed_salt
+    password_to_check = hashlib.sha256(new_password.encode("ascii")).hexdigest()
+    combined_salt_and_password = ''.join([password_to_check, hashed_salt])
+
+    return combined_salt_and_password == hashed_password
+
+
