232c978aa6f2c2210d89e6b9052e309aefe9d016
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-14 13:57:22 +0200

    Created hashing func and veryfying one

--- users/utils.py
+++ users/utils.py
@@ -2,5 +2,9 @@ import hashlib
 
 
 def hash_password_without_salt(password: str) -> str:
-    hash_password = hashlib.sha256(password.encode("ascii"))
-    return hash_password.hexdigest()
+    hash_password = hashlib.sha256(password.encode("ascii")).hexdigest()
+    return hash_password
+
+def verify_password_without_salt(password: str, hash_password: str) -> bool:
+    input_password = hashlib.sha256(password.encode("ascii")).hexdigest()
+    return input_password == hash_password
