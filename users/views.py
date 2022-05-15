eea825f5fc5fbfcc476e098d4eda0b3a5de91d54
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-15 12:31:17 +0200

    Created users listing

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from users.forms import SignUpForm
 
@@ -13,4 +15,6 @@ class SignUpView(CreateView):
 class UserLoginView(LoginView):
     template_name = 'login.html'
 
-
+class UserListView(LoginRequiredMixin, ListView):
+    template_name = 'user_list.html'
+    model = User
